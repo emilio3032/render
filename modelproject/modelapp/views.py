79b9efc6057b914dca580935e3fa8222f2e0f657
Mr.Emilio <afzalbinsaleem14@gmail.com>
2023-07-12 11:07:03 +0530

    add3

---
+++ modelproject/modelapp/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from . models import place
+# Create your views here.
+def home(request):
+    obj=place.objects.all()
+    return render(request,"index.html",{'result':obj})
--- renderproject1/renderapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RenderappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'renderapp'
